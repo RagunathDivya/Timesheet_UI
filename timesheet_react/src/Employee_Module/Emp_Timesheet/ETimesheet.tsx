e5ee98b169108c9bc6f6924ed21bde9d1fa7f11a
SubhashR8 <126658900+SubhashR8@users.noreply.github.com>
2023-04-27 11:46:06 +0530

    Excel link

--- timesheet_react/src/Employee_Module/Emp_Timesheet/ETimesheet.tsx
+++ timesheet_react/src/Employee_Module/Emp_Timesheet/ETimesheet.tsx
@@ -429,7 +429,7 @@ function AddTimesheet() {
       const url = window.URL.createObjectURL(new Blob([response.data]));
       const link = document.createElement("a");
       link.href = url;
-      link.setAttribute("download", "Timeheet.xlsx"); //or any other extension
+      // link.setAttribute("download", "Timeheet.xlsx"); //or any other extension
       document.body.appendChild(link);
       link.click();
     });
